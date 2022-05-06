408d957cb2bb14c0fd4d9f30d5ecf69e201a52fd
Tim Holmes <tpholmes7@gmail.com>
2022-05-06 08:21:04 -0500

    Auto Update: Fri May  6 08:21:04 CDT 2022

--- backup.sh
+++ backup.sh
@@ -2,3 +2,19 @@ cp ~/.vimrc .
 cp ~/.zshrc .
 cp ~/.config/starship.toml .
 cp /Users/$USER/Library/Application\ Support/Code/User/settings.json .
+
+
+
+# push code to gh if config change
+git add .
+set +e
+git status | grep modified
+if [ $? -eq 0 ]
+then
+    set -e
+    git commit -am "Auto Update: $(date)"
+    git push
+else
+    set -e
+    echo "No changes since last run"
+fi
